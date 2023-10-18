1efcde5d8b1d41482f5100e5e5e57ce467ff8fcf
Егор Максимов <yegor_maksimov2008@mail.ru>
2023-10-18 19:21:49 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//using System.Configuration;
+//using System.Collections.Specialized;
+//string sAttr;
+//sAttr = ConfigurationManager.AppSettings.Get("Key0"); //Only one key-value
+//Console.WriteLine("The value of Key0 is " + sAttr);
+//NameValueCollection sAll;
+//sAll = ConfigurationManager.AppSettings; //All keys-values
+//foreach (string s in sAll.AllKeys)
+//    Console.WriteLine("Key: " + s + " Value: " + sAll.Get(s));
+
+//Console.ReadLine();
+
+//EXAMPLE OF CONFIGURATION FILE IN C#
+
+
+
+
+//CODING TRACKER
